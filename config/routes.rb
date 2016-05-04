789892eff718c5025fc6370d1b0aa3c9607f29da
KimYiJun <ybc864@naver.com>
2016-05-04 10:19:46 +0000

    Rails Framework

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Testapp::Application.routes.draw do
+  get "chat/index"
+  get "chat/new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
