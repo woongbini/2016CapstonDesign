b6421b9a2b596553cab6fd3c73fd2a1671f256d6
YiJunKim <ybc864@naver.com>
2016-05-08 13:26:33 +0000

    mainpage.html.erb 수정중1

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Testapp::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  root :to => "mainpage#mainpage"
+  # You can have the iroot of your site routed with "root"
+    root :to => "mainpage#mainpage"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
