018a8c39853246af7090e9294df455c4fa0c1f85
YiJunKim <ybc864@naver.com>
2016-05-08 11:34:45 +0000

    mainpage.index.erb를 main으로 설정후에 불필요한 파일 정리

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Testapp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  #root 'welcome#index'
+  root :to => "mainpage#mainpage"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
