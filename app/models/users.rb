f55c9dbe4a71618c0004ce14296db27d6159050d
Woong Bin Kim <gamebook1004@gmail.com>
2016-05-14 12:44:49 +0000

    DB정리

--- app/models/users.rb
+++
@@ -1,5 +0,0 @@
-class Users < ActiveRecord::Base
-#	validates :email
-#	validates :password
-#	validates :phone
-end
--- db/migrate/20160514124341_create_users.rb
+++ db/migrate/20160514124341_create_users.rb
@@ -1,10 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-			t.string :email, null:false
-			t.string :password, null:false
-			t.string :phone, null:false
-		
+
       t.timestamps
     end
   end
