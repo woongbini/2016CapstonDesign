2b0c1a97851eda83a55b7d753c0d6b4154c0690c
Woong Bin Kim <gamebook1004@gmail.com>
2016-05-14 22:04:58 +0900

    수정후 정리2

--- db/migrate/20160514124341_create_users.rb
+++ db/migrate/20160514124341_create_users.rb
@@ -1,8 +1,13 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :email
+      t.string :password
+      t.string :phone
+      t.string :nickname
 
       t.timestamps
+
     end
   end
 end
