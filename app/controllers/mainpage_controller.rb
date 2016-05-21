67e546f83286916c9f60a8d5d707504ac4b1bf3e
Woong Bin Kim <gamebook1004@gmail.com>
2016-05-21 11:22:12 +0000

    카테고리별로 게시글 보기 기능 완성

--- app/controllers/mainpage_controller.rb
+++ app/controllers/mainpage_controller.rb
@@ -1,12 +1,12 @@
 class MainpageController < ApplicationController
   def mainpage
-    @posts = Post.all #Post 테이블에 저장된 모든 행들을 읽어들인뒤, @posts 변수에 저장
+    @posts_vol = Post.where(category: "Voluntal") #Post 테이블에 저장된 Voluntal 행들을 읽어들임
+    @posts_tal = Post.where(category: "Talent") #Post 테이블에 저장된 Talent 행들을 읽어들임
   end
 
   def addmemberpage
   end
   
   def page_title
-    "main"
   end
 end
