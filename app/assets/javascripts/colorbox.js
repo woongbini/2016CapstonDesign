ab8522cc5141597e3b49f9139fb3b8a8ec7df656
KimYiJun <ybc864@naver.com>
2016-05-25 07:16:55 +0000

    colorbox

---
+++ app/assets/javascripts/colorbox.js
@@ -0,0 +1,5 @@
+// JavaScript Document
+$(document).ready(function() {
+	$(".group1").colorbox({rel:'group1'});
+    $('.color_box1').colorbox({iframe:true, width:720, height:800, scrolling:true});
+});
